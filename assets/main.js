69c0f28a38d99e8211a1e91b788c3aefd1516938
djfordz <djfordz@gmail.com>
2015-12-06 20:38:03 -0600

    created sticky header

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,11 @@
-
+(function($) {
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 1) {
+            $('header').addClass('sticky');
+            $('header').removeClass('site-header');
+        } else {
+            $('header').removeClass('sticky');
+            $('header').addClass('site-header');
+        }
+    })
+})(jQuery);
