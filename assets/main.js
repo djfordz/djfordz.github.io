ae8d883fc03d204c90aad357bc5e739f72d92819
djfordz <djfordz@gmail.com>
2015-12-06 22:58:53 -0600

    decided to make mobile header primary

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,1 @@
-(function($) {
-    $(window).scroll(function() {
-        if($(this).scrollTop() > 1) {
-            $('header').addClass('sticky');
-            $('header').removeClass('site-header');
-            if($(window).width() < 500) {
-                $('.site-title').hide();
-            }
-        } else {
-            $('header').removeClass('sticky');
-            $('header').addClass('site-header');
-            $('.site-title').show();
-        }
 
-    })
-})(jQuery);
