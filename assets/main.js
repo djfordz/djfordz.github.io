58ab691ea3a578942d22301ba339577e3be84b7e
djfordz <djfordz@gmail.com>
2015-12-06 22:15:03 -0600

    fixed header issue on mobile

--- assets/main.js
+++ assets/main.js
@@ -3,9 +3,14 @@
         if($(this).scrollTop() > 1) {
             $('header').addClass('sticky');
             $('header').removeClass('site-header');
+            if($(window).width() < 500) {
+                $('.site-title').hide();
+            }
         } else {
             $('header').removeClass('sticky');
             $('header').addClass('site-header');
+            $('.site-title').show();
         }
+
     })
 })(jQuery);
