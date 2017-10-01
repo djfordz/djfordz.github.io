ed1095ed9066a019967b038edcd2829e2c47433b
David Ford <djfordz@gmail.com>
2017-10-01 08:09:26 -0500

    changed posts image center from js hack to css

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,1 @@
-var el = [].slice.call(document.querySelectorAll('p'));
 
-el.forEach(function(value) {
-    var children = [].slice.call(value.childNodes);
-    children.map(function(child) {
-        if (child.tagName == 'IMG') {
-            child.parentNode.style.textAlign = 'center';
-            child.parentNode.style.marginTop = '40px';
-        }
-    });
-});
